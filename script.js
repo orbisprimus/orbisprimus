9bf548a58b7579db98855448c3ac847b494465a2
Therese Selberg <these002@student.liu.se>
2024-06-09 18:30:12 +0200

    first version

---
+++ script.js
@@ -0,0 +1,37 @@
+
+class AppHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class='header'>
+                <img src='images/header.png' alt='Orbis Primus'>
+            </div>
+        `
+    }
+}
+window.customElements.define('orbis-header', AppHeader)
+
+class AppNav extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <nav id='navbar'>
+                <a href='/'>Om oss</a>
+                <a href='boka.html'>Boka oss</a>
+                <a href='ansok.html'>Ansök</a>
+                <a href='kontakt.html'>Kontakta oss</a>
+            </nav>
+        `
+    }
+}
+window.customElements.define('orbis-nav', AppNav)
+
+class AppFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <footer>
+                <a href='http://www.instagram.com/orbisprimus'><img src='images/instagram.png' alt='orbisprimus på Instagram'></a>
+                <a href='http://www.facebook.com/orbisprimus'><img src='images/facebook.png' alt='orbisprimus på Facebook'></a>
+            </footer>
+        `
+    }
+}
+window.customElements.define('orbis-footer', AppFooter)
