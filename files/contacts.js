c8eb0b17144fa76e0afcdbd9df034d032d8c62ed
Therese Selberg <tselberg99@gmail.com>
2025-03-19 21:15:46 +0100

    only persons with email

--- files/contacts.js
+++ files/contacts.js
@@ -5,18 +5,18 @@ function createContactCard(person) {
     let h2 = document.createElement("h2");
 
     div.className = "contactCard";
-    img.src = "./files/images/contacts/" + person.Bild;
-    img.alt = person.Namn;
-    h1.innerText = person.Namn;
-    h2.innerText = person.Post;
+    img.src = "./files/images/contacts/" + person.bild;
+    img.alt = person.namn;
+    h1.innerText = person.namn;
+    h2.innerText = person.post;
 
     div.appendChild(img);
     div.appendChild(h1);
     div.appendChild(h2);
 
-    if (person.Email) {
+    if (person.email) {
         let a = document.createElement("a");
-        let email = person.Email;
+        let email = person.email;
         a.href = "mailto:" + email;
         a.innerText = email;
         div.appendChild(a);
