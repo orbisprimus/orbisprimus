659272ade56a525a0167ee5fdcee81c5926e8ec7
Therese Selberg <tselberg99@gmail.com>
2025-03-28 19:33:55 +0100

    multiple albums, only need to specify folder per album

--- files/album.js
+++ files/album.js
@@ -19,59 +19,72 @@ const enableHideModalWithKeyboard = function() {
 const createImageDiv = image => {
     let img = document.createElement("img");
     img.src = '/files/images/album/' + image.filename;
-    img.alt = image.caption;
     img.onclick = 'displayImage()';
-
-    const p = document.createElement("p");
-    p.innerText = image.caption;
 
     img.onclick = function() {
         let modal = document.getElementById("modal");
-        let caption = document.getElementById("caption");
         let modalImg = document.getElementById("modal-img");
         modal.style.display = "block";
         modalImg.src = img.src;
-        caption.innerText = img.alt;
     }
 
-    if (image.header) {
+    if (image.headertitle) {
         img.classList.add("header-img");
     }
 
     return img;
 };
 
-const createAlbumDiv = album => {
+const createAlbumDiv = (images) => {
     let div = document.createElement("div");
-    let h2 = document.createElement("h2");
+    div.className = "sub-album";
 
-    h2.innerText = album.title;
-    div.appendChild(h2);
-
-    for (image in album.images) {
-        div.appendChild(createImageDiv(album.images[image]));
+    for (i in images) {
+        div.appendChild(createImageDiv({"filename": images[i]}));
     }
 
     return div;
 };
+
+const getImagePaths = async (folder) => {
+    const response = await fetch('/files/images/album/' + folder);
+    return response.body.getReader().read().then(res => {
+        let uint8array = res.value;
+        let page = new TextDecoder().decode(uint8array);
+        const rx = new RegExp("(" + folder + "/.+?)\"", "g");
+        let images = new Array();
+        page.matchAll(rx).forEach(img => {
+            images.push(img[1]);
+        });
+        return images.sort();
+    });
+
+}
 
 const addImages = async () => {
     const div = document.getElementById('album');
     if (!div) {
         return;
     }
+
     const response = await fetch('/files/images/album.json');
     const albums = await response.json();
     for (index in albums) {
-        if (albums[index].header) {
+        if (albums[index].headertitle) {
             let subDiv = document.createElement("div");
             let h1 = document.createElement("h1");
-            h1.innerText = albums[index].caption;
+            h1.innerText = albums[index].headertitle;
             subDiv.appendChild(createImageDiv(albums[index]));
             subDiv.appendChild(h1);
             div.appendChild(subDiv);
         } else {
-            div.appendChild(createAlbumDiv(albums[index]));
+            let title = albums[index].title;
+            getImagePaths(albums[index].folder).then(paths => {
+                let h2 = document.createElement("h2");
+                h2.innerText = title;
+                div.appendChild(h2);
+                div.appendChild(createAlbumDiv(paths));
+            });
         }
     }
 };
